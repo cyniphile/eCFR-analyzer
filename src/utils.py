a225fd4671b79dc21a6e1619b0e9813678d5ade4
Luke Schiefelbein <luke.schiefelbein@gmail.com>
2025-04-17 14:32:07 -0400

    Revert "Revert "improve titles""
    This reverts commit c2f8c667c8794637fea646d507931c3cd4175390.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import requests
 from pathlib import Path
 import os
+import json
 
 BASE_URL = "https://www.ecfr.gov/"
 TITLES_LIST_URL = f"{BASE_URL}/api/versioner/v1/titles"
@@ -15,6 +16,22 @@ def get_titles_list(url=TITLES_LIST_URL):
     if response.status_code != 200:
         raise Exception(f"Failed to get titles list: {response.status_code}")
     return response.json()
+
+
+def get_or_load_titles_list():
+    """
+    Get the list of titles from the ECFR API or load it from a local file
+    """
+    # Check if the file exists
+    if os.path.exists(app_data_dir / "titles.json"):
+        with open(data_dir / "titles.json", "r") as f:
+            return json.load(f)
+    else:
+        # If not, fetch it from the API and save it
+        titles = get_titles_list()
+        with open(app_data_dir / "titles.json", "w") as f:
+            json.dump(titles, f)
+        return titles
 
 
 def get_project_root() -> Path:
